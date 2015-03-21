e19e2d8032fdce33d9dc0ea61650da5f06eba376
Adam Haag <ahaag@galapagos-02.cs.wisc.edu>
2015-03-21 12:43:09 -0500

    Breakit

---
+++ user/breakit.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int* p = 0;
+  printf(1, "%p\n",*p);
+  exit();
+}
--- user/makefile.mk
+++ user/makefile.mk
@@ -16,7 +16,8 @@ USER_PROGS := \
 	tester\
 	usertests\
 	wc\
-	zombie
+	zombie\
+	breakit
 
 USER_PROGS := $(addprefix user/, $(USER_PROGS))
 
